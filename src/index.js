20f2d973c1b675a4043692ebe5395ba51faced4a
Robert Buels <rbuels@gmail.com>
2018-04-28 18:27:28 -0700

    calculate size and start for linear index blocks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import indexParser from './parsers'
 export default function read(input) {
   if (Buffer.isBuffer(input)) {
     const data = indexParser.parse(input)
+    data.data.chromosomes.forEach(chr => {
+      let currentPosition = chr.startPosition
+      chr.blocks.forEach(block => {
+        const blockPosition = block.position
+        delete block.position
+        block.size = blockPosition - currentPosition
+        block.start = currentPosition
+        currentPosition = blockPosition
+      })
+    })
     return data
   }
 
