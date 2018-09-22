57aa6379e3e4be4df3a15ba4c13c75e47c419c4a
Garrett Stevens <stevens.garrett.j@gmail.com>
2018-09-21 20:04:56 -0400

    initial basic functionality of tribbleIndexedFile

---
+++ src/tribbleIndexedFile.js
@@ -0,0 +1,251 @@
+const LRU = require('lru-cache')
+const LocalFile = require('./localFile')
+const read = require('./tribble')
+
+// function timeout(time) {
+//   return new Promise(resolve => {
+//     setTimeout(resolve, time)
+//   })
+// }
+
+class TribbleIndexedFile {
+  /**
+   * @param {object} args
+   * @param {string} [args.path]
+   * @param {filehandle} [args.filehandle]
+   * @param {string} [args.tribblePath]
+   * @param {filehandle} [args.tribbleFilehandle]
+   * @param {string} [args.metaChar] character that denotes the beginning of a header line
+   * @param {number} [args.chunkSizeLimit] maximum number of bytes to fetch in a single `getLines` call.
+   * default 2MiB
+   * @param {number} [args.yieldLimit] maximum number of lines to parse without yielding.
+   * this avoids having a large read prevent any other work getting done on the thread.  default 300 lines.
+   * @param {function} [args.renameRefSeqs] optional function with sig `string => string` to transform
+   * reference sequence names for the purpose of indexing and querying. note that the data that is returned is
+   * not altered, just the names of the reference sequences that are used for querying.
+   * @param {number} [args.blockCacheSize] maximum size in bytes of the block cache. default 5MB
+   */
+  constructor({
+    path,
+    filehandle,
+    tribblePath,
+    tribbleFilehandle,
+    metaChar = '#',
+    chunkSizeLimit = 2000000,
+    yieldLimit = 300,
+    renameRefSeqs = n => n,
+    blockCacheSize = 5 * 2 ** 20,
+  }) {
+    if (filehandle) this.filehandle = filehandle
+    else if (path) this.filehandle = new LocalFile(path)
+    else throw new TypeError('must provide either filehandle or path')
+
+    if (tribbleFilehandle) this.tribbleFilehandle = tribbleFilehandle
+    else if (tribblePath) this.tribbleFilehandle = new LocalFile(tribblePath)
+    else if (path) {
+      this.tribbleFilehandle = new LocalFile(`${path}.idx`)
+    } else {
+      throw new TypeError(
+        'must provide one of tribbleFilehandle or tribblePath',
+      )
+    }
+
+    this.metaChar = metaChar
+    this.chunkSizeLimit = chunkSizeLimit
+    this.yieldLimit = yieldLimit
+    this.renameRefSeqCallback = renameRefSeqs
+    this.blockCache = LRU({
+      max: Math.floor(blockCacheSize / (1 << 16)),
+      length: n => n.length,
+    })
+  }
+
+  async _loadIndex() {
+    if (!this.index) {
+      const bytes = await this.tribbleFilehandle.readFile()
+      this.index = read(bytes)
+    }
+    this.index.renamedRefToRef = {}
+    Object.keys(this.index.chromosomeEntries).forEach(ref => {
+      this.index.renamedRefToRef[this.renameRefSeqCallback(ref)] = ref
+    })
+    return this.index
+  }
+
+  async getLines(ref, min, max, lineCallback) {
+    const index = await this._loadIndex()
+    const originalRef = this.index.renamedRefToRef[ref]
+    const blocks = index.getBlocks(originalRef, min, max)
+    if (!blocks) {
+      throw new Error(`Error in index fetch (${[ref, min, max].join(',')})`)
+    }
+    // check the chunks for any that are over the size limit.  if
+    // any are, don't fetch any of them
+    for (let i = 0; i < blocks.length; i += 1) {
+      const size = blocks[i].length
+      if (size > this.chunkSizeLimit) {
+        throw new Error(
+          `Too much data. Chunk size ${size.toLocaleString()} bytes exceeds chunkSizeLimit of ${this.chunkSizeLimit.toLocaleString()}.`,
+        )
+      }
+    }
+    const results = []
+    for (let blockNum = 0; blockNum < blocks.length; blockNum += 1) {
+      results.push(this._getLinesFromBlock(blocks[blockNum]))
+    }
+    const retrievedBlocks = await Promise.all(results)
+    retrievedBlocks.forEach(block => {
+      block.forEach(line => {
+        const lineCheck = this.checkLine(ref, min, max, line)
+        if (lineCheck.overlaps) lineCallback(line)
+      })
+    })
+  }
+
+  _getLinesFromBlock(block) {
+    return this._getBlockCache(JSON.stringify(block), async () => {
+      const buffer = Buffer.alloc(block.length)
+      await this.filehandle.read(buffer, 0, block.length, block.offset)
+      const lines = buffer.toString('utf8').split('\n')
+      // remove the last line, since it will be either empty or partial
+      lines.pop()
+      return lines
+    })
+  }
+
+  /**
+   * @param {object} metadata metadata object from the parsed index,
+   * containing columnNumbers, metaChar, and maxColumn
+   * @param {string} regionRefName
+   * @param {number} regionStart region start coordinate (0-based-half-open)
+   * @param {number} regionEnd region end coordinate (0-based-half-open)
+   * @param {array[string]} line
+   * @returns {object} like `{startCoordinate, overlaps}`. overlaps is boolean,
+   * true if line is a data line that overlaps the given region
+   */
+  checkLine(regionRefName, regionStart, regionEnd, line) {
+    // skip meta lines
+    if (line.charAt(0) === this.metaChar) return { overlaps: false }
+
+    // this code is kind of complex, but it is fairly fast.
+    // basically, we want to avoid doing a split, because if the lines are really long
+    // that could lead to us allocating a bunch of extra memory, which is slow
+
+    let currentColumnNumber = 0
+    let currentColumnStart = 0
+    let startCoordinate
+    for (let i = 0; i < line.length; i += 1) {
+      if (line[i] === '\t') {
+        if (currentColumnNumber === 0) {
+          let refName = line.slice(currentColumnStart, i)
+          refName = this.renameRefSeqCallback(regionRefName)
+          if (refName !== regionRefName) return { overlaps: false }
+        } else if (currentColumnNumber === 1) {
+          startCoordinate = parseInt(line.slice(currentColumnStart, i), 10)
+          // we convert to 0-based-half-open
+          if (startCoordinate >= regionEnd) return { overlaps: false }
+          // assume the feature is 1 bp long
+          if (startCoordinate + 1 <= regionStart) return { overlaps: false }
+          return { startCoordinate, overlaps: true }
+        }
+        currentColumnStart = i + 1
+        currentColumnNumber += 1
+      }
+    }
+    return { overlaps: false }
+  }
+
+  _getBlockCache(cacheKey, fillCallback) {
+    const cachedPromise = this.blockCache.get(cacheKey)
+    if (cachedPromise) return cachedPromise
+
+    const freshPromise = fillCallback()
+    this.blockCache.set(cacheKey, freshPromise)
+    return freshPromise
+  }
+
+  // async getMetadata() {
+  //   return this.index.getMetadata()
+  // }
+
+  // /**
+  //  * get a buffer containing the "header" region of
+  //  * the file, which are the bytes up to the first
+  //  * non-meta line
+  //  *
+  //  * @returns {Promise} for a buffer
+  //  */
+  // async getHeaderBuffer() {
+  //   const { firstDataLine, metaChar, maxBlockSize } = await this.getMetadata()
+  //   const maxFetch =
+  //     firstDataLine && firstDataLine.blockPosition
+  //       ? firstDataLine.blockPosition + maxBlockSize
+  //       : maxBlockSize
+  //   // TODO: what if we don't have a firstDataLine, and the header
+  //   // actually takes up more than one block? this case is not covered here
+
+  //   let bytes = await this._readRegion(0, maxFetch)
+  //   // trim off lines after the last non-meta line
+  //   if (metaChar) {
+  //     // trim backward from the end
+  //     let lastNewline = -1
+  //     const newlineByte = '\n'.charCodeAt(0)
+  //     const metaByte = metaChar.charCodeAt(0)
+  //     for (let i = 0; i < bytes.length; i += 1) {
+  //       if (bytes[i] === newlineByte) {
+  //         lastNewline = i
+  //         i += 1
+  //         if (bytes[i] !== metaByte) break
+  //       }
+  //     }
+  //     bytes = bytes.slice(0, lastNewline + 1)
+  //   }
+  //   return bytes
+  // }
+
+  // /**
+  //  * get a string containing the "header" region of the
+  //  * file, is the portion up to the first non-meta line
+  //  *
+  //  * @returns {Promise} for a string
+  //  */
+  // async getHeader() {
+  //   const bytes = await this.getHeaderBuffer()
+  //   return bytes.toString('utf8')
+  // }
+
+  // /**
+  //  * get an array of reference sequence names, in the order in which
+  //  * they occur in the file.
+  //  *
+  //  * reference sequence renaming is not applied to these names.
+  //  *
+  //  * @returns {Promise} for an array of string sequence names
+  //  */
+  // async getReferenceSequenceNames() {
+  //   const metadata = await this.getMetadata()
+  //   return metadata.refIdToName
+  // }
+
+  //   /**
+  //    * @param {object} metadata metadata object from the parsed index,
+  //    * containing columnNumbers, metaChar, and maxColumn
+  //    * @param {string} regionRefName
+  //    * @param {number} regionStart region start coordinate (0-based-half-open)
+  //    * @param {number} regionEnd region end coordinate (0-based-half-open)
+  //    * @param {array[string]} line
+  //    * @returns {object} like `{startCoordinate, overlaps}`. overlaps is boolean,
+  //    * true if line is a data line that overlaps the given region
+  //    */
+
+  //   /**
+  //    * return the approximate number of data lines in the given reference sequence
+  //    * @param {string} refSeq reference sequence name
+  //    * @returns {Promise} for number of data lines present on that reference sequence
+  //    */
+  //   async lineCount(refSeq) {
+  //     return this.index.lineCount(refSeq)
+  //   }
+}
+
+module.exports = TribbleIndexedFile
---
+++ test/tribbleIndexedFile.test.js
@@ -0,0 +1,267 @@
+const TribbleIndexedFile = require('../src/tribbleIndexedFile')
+
+// class RecordCollector {
+//   constructor() {
+//     this.clear()
+//     this.callback = (line, fileOffset) => {
+//       this.records.push({ line, fileOffset })
+//       this.length += 1
+//     }
+//   }
+//   forEach(cb) {
+//     this.records.forEach(cb)
+//   }
+//   clear() {
+//     this.records = []
+//     this.length = 0
+//   }
+// }
+describe('tabix file', () => {
+  const lines = []
+  it('can read ctgA:1000..4000', async () => {
+    const f = new TribbleIndexedFile({
+      path: require.resolve('./data/volvox.test.vcf'),
+      tribblePath: require.resolve('./data/volvox.test.vcf.idx'),
+      yieldLimit: 10,
+      renameRefSeqs: n => n.replace('contig', 'ctg'),
+    })
+    // const items = new RecordCollector()
+    // await f.getLines('ctgA', 1000, 4000, items.callback)
+    await f.getLines('ctgA', 1000, 4000, l => lines.push(l))
+    console.log(lines)
+  })
+})
+//   expect(items.length).toEqual(8)
+//   items.forEach(({ line, fileOffset }) => {
+//     line = line.split('\t')
+//     expect(line[0]).toEqual('contigA')
+//     expect(parseInt(line[1], 10)).toBeGreaterThan(999)
+//     expect(parseInt(line[1], 10)).toBeLessThan(4001)
+//     expect(fileOffset).toBeGreaterThanOrEqual(0)
+//   })
+
+//   items.clear()
+//   await f.getLines('ctgA', 3000, 3000, items.callback)
+//   expect(items.length).toEqual(0)
+//   items.clear()
+//   await f.getLines('ctgA', 2999, 3000, items.callback)
+//   expect(items.length).toEqual(1)
+//   items.clear()
+//   await f.getLines('ctgA', 3000, 3001, items.callback)
+//   expect(items.length).toEqual(0)
+
+//   const headerString = await f.getHeader()
+//   expect(headerString.length).toEqual(10431)
+//   expect(headerString[headerString.length - 1]).toEqual('\n')
+
+//   expect(await f.getMetadata()).toEqual({
+//     columnNumbers: { end: 0, ref: 1, start: 2 },
+//     coordinateType: '1-based-closed',
+//     maxBlockSize: 1 << 16,
+//     format: 'VCF',
+//     metaChar: '#',
+//     firstDataLine: new VirtualOffset(0, 10431),
+//     refIdToName: ['ctgA'],
+//     refNameToId: { ctgA: 0 },
+//     skipLines: 0,
+//     maxBinNumber: 37449,
+//   })
+// })
+//   it('can count lines with TBI', async () => {
+//     const f = new TabixIndexedFile({
+//       path: require.resolve('./data/volvox.test.vcf.gz'),
+//       tbiPath: require.resolve('./data/volvox.test.vcf.gz.tbi'),
+//       yieldLimit: 10,
+//     })
+//     expect(await f.lineCount('contigA')).toEqual(109)
+//     expect(await f.lineCount('nonexistent')).toEqual(-1)
+//   })
+//   it('can count lines with CSI', async () => {
+//     const f = new TabixIndexedFile({
+//       path: require.resolve('./data/volvox.test.vcf.gz'),
+//       csiPath: require.resolve('./data/volvox.test.vcf.gz.csi'),
+//       yieldLimit: 10,
+//     })
+//     expect(await f.lineCount('contigA')).toEqual(109)
+//     expect(await f.lineCount('nonexistent')).toEqual(-1)
+//   })
+//   it("can't count lines without pseudo-bin", async () => {
+//     const f = new TabixIndexedFile({
+//       path: require.resolve('./data/volvox.test.vcf.gz'),
+//       tbiPath: require.resolve('./data/volvox.test.vcf.gz.tbi.no_pseudo'),
+//       yieldLimit: 10,
+//     })
+//     expect(await f.lineCount('contigA')).toEqual(-1)
+//   })
+//   it('handles invalid input', async () => {
+//     const f = new TabixIndexedFile({
+//       path: require.resolve('./data/volvox.test.vcf.gz'),
+//       tbiPath: require.resolve('./data/volvox.test.vcf.gz.tbi'),
+//       yieldLimit: 10,
+//     })
+//     let err
+//     const catchErr = e => {
+//       err = e
+//     }
+//     await f.getLines('foo', 32, 24).catch(catchErr)
+//     expect(err.toString()).toContain('invalid start')
+//     err = undefined
+//     await f.getLines().catch(catchErr)
+//     expect(err.toString()).toContain('reference')
+//     await f.getLines('foo', 23, 45).catch(catchErr)
+//     expect(err.toString()).toContain('callback')
+//   })
+//   it('can query volvox.sort.gff3.gz.1', async () => {
+//     const f = new TabixIndexedFile({
+//       path: require.resolve('./data/volvox.sort.gff3.gz.1'),
+//       tbiPath: require.resolve('./data/volvox.sort.gff3.gz.tbi'),
+//     })
+
+//     const headerString = await f.getHeader()
+//     expect(headerString[headerString.length - 1]).toEqual('\n')
+//     expect(headerString.length).toEqual(130)
+
+//     const lines = []
+//     await f.getLines('ctgB', 0, Infinity, l => lines.push(l))
+//     expect(lines.length).toEqual(4)
+//     expect(lines[3]).toEqual(
+//       'ctgB	example	remark	4715	5968	.	-	.	Name=f05;Note=ああ、この機能は、世界中を旅しています！',
+//     )
+//     lines.length = 0
+//     await f.getLines('ctgA', 10000000, Infinity, l => lines.push(l))
+//     expect(lines.length).toEqual(0)
+//     lines.length = 0
+//     await f.getLines('ctgA', 0, Infinity, l => lines.push(l))
+//     expect(lines.length).toEqual(237)
+//     lines.length = 0
+//     await f.getLines('ctgB', 0, Infinity, l => lines.push(l))
+//     expect(lines.length).toEqual(4)
+//     lines.length = 0
+//     await f.getLines('ctgB', 0, 4715, l => lines.push(l))
+//     expect(lines.length).toEqual(4)
+//     lines.length = 0
+//     await f.getLines('ctgB', 1, 4714, l => lines.push(l))
+//     expect(lines.length).toEqual(3)
+//   })
+//   it('can query gvcf.vcf.gz', async () => {
+//     const f = new TabixIndexedFile({
+//       path: require.resolve('./data/gvcf.vcf.gz'),
+//     })
+
+//     const headerString = await f.getHeader()
+//     expect(headerString.length).toEqual(53)
+//     expect(headerString[headerString.length - 1]).toEqual('\n')
+
+//     const lines = []
+//     await f.getLines('ctgB', 0, Infinity, l => lines.push(l))
+//     expect(lines.length).toEqual(0)
+
+//     await f.getLines('ctgA', -2, 3000, l => lines.push(l))
+//     expect(lines.length).toEqual(0)
+//     await f.getLines('ctgA', -50, -20, l => lines.push(l))
+//     expect(lines.length).toEqual(0)
+//     await f.getLines('ctgA', 4000, 5000, l => lines.push(l))
+//     expect(lines.length).toEqual(7)
+//     lines.length = 0
+//     await f.getLines('ctgA', 4370, 4371, l => lines.push(l))
+//     expect(lines.length).toEqual(0)
+//     lines.length = 0
+//     await f.getLines('ctgA', 4369, 4370, l => lines.push(l))
+//     expect(lines.length).toEqual(1)
+//   })
+
+//   it('can query out.gff.gz with a TBI index', async () => {
+//     const f = new TabixIndexedFile({
+//       path: require.resolve('./data/out.gff.gz'),
+//     })
+
+//     const headerString = await f.getHeader()
+//     expect(headerString.length).toEqual(132)
+//     expect(headerString[headerString.length - 1]).toEqual('\n')
+
+//     expect(await f.getReferenceSequenceNames()).toEqual(['NC_000001.11'])
+
+//     let lineCount = 0
+//     const lines = new RecordCollector()
+//     await f.getLines('NC_000001.11', 30000, 55000, lines.callback)
+//     lines.forEach(({ line, fileOffset }) => {
+//       const fields = line.split('\t')
+//       lineCount += 1
+//       expect(fields[0]).toEqual('NC_000001.11')
+//       expect(parseInt(fields[3], 10)).toBeLessThan(55000)
+//       expect(parseInt(fields[4], 10)).toBeGreaterThan(3000)
+//       expect(fileOffset).toBeGreaterThanOrEqual(0)
+//     })
+//     expect(lineCount).toEqual(23)
+//     expect(lines.records[0].line).toEqual(
+//       'NC_000001.11	RefSeq	region	1	248956422	.	+	.	Dbxref=taxon:9606;Name=1;chromosome=1;gbkey=Src;genome=chromosome;mol_type=genomic DNA',
+//     )
+//     expect(lines.records[22].line).toEqual(
+//       'NC_000001.11	Gnomon	exon	53282	53959	.	+	.	Parent=lnc_RNA3;Dbxref=GeneID:105379212,Genbank:XR_948874.1;gbkey=ncRNA;gene=LOC105379212;product=uncharacterized LOC105379212;transcript_id=XR_948874.1',
+//     )
+//   })
+
+//   it('can query test.vcf.gz with a CSI index', async () => {
+//     const f = new TabixIndexedFile({
+//       path: require.resolve('./data/test.vcf.gz'),
+//       csiPath: require.resolve('./data/test.vcf.gz.csi'),
+//     })
+
+//     const headerString = await f.getHeader()
+//     expect(headerString.length).toEqual(2560)
+//     expect(headerString[headerString.length - 1]).toEqual('\n')
+
+//     const lines = new RecordCollector()
+//     await f.getLines('ctgB', 0, Infinity, lines.callback)
+//     expect(lines.length).toEqual(0)
+
+//     await f.getLines('ctgA', -2, 3000, lines.callback)
+//     expect(lines.length).toEqual(0)
+//     await f.getLines('ctgA', -50, -20, lines.callback)
+//     expect(lines.length).toEqual(0)
+//     await f.getLines('1', 4000, 5000, lines.callback)
+//     expect(lines.length).toEqual(0)
+//     lines.clear()
+//     await f.getLines('1', 1206810423, 1206810423, lines.callback)
+//     expect(lines.length).toEqual(0)
+//     lines.clear()
+//     await expect(
+//       f.getLines('1', 1206808844, 12068500000, lines.callback),
+//     ).rejects.toThrow(/query .* is too large for current binning scheme/)
+//     lines.clear()
+//     await f.getLines('1', 1206810422, 1206810423, lines.callback)
+//     expect(lines.length).toEqual(1)
+//     expect(lines.records[0].line).toEqual(
+//       '1	1206810423	.	T	A	25	.	DP=19;VDB=0.0404;AF1=0.5;AC1=1;DP4=3,7,3,6;MQ=37;FQ=28;PV4=1,1,1,0.27	GT:PL:GQ	0/1:55,0,73:58',
+//     )
+//     lines.clear()
+//     await f.getLines('1', 1206810423, 1206810424, lines.callback)
+//     expect(lines.length).toEqual(0)
+//     await f.getLines('1', 1206810423, 1206849288, lines.callback)
+//     expect(lines.length).toEqual(36)
+//     expect(lines.records[35].line).toEqual(
+//       '1	1206849288	.	G	A	106	.	DP=23;VDB=0.0399;AF1=1;AC1=2;DP4=0,0,16,7;MQ=35;FQ=-96	GT:PL:GQ	1/1:139,69,0:99',
+//     )
+//     lines.clear()
+//     await f.getLines('1', 1206810423, 1206810424, lines.callback)
+//     expect(lines.length).toEqual(0)
+//   })
+
+//   it('can fetch the entire header for a very large vcf header', async () => {
+//     const f = new TabixIndexedFile({
+//       path: require.resolve('./data/large_vcf_header.vcf.gz'),
+//     })
+
+//     const headerString = await f.getHeader()
+//     const lastBitOfLastHeaderLine =
+//       'CN_105715_AGL\tCDC_QG-1_AGL\tCDC_SB-1_AGL\n'
+//     expect(
+//       headerString.slice(
+//         headerString.length - lastBitOfLastHeaderLine.length,
+//         headerString.length,
+//       ),
+//     ).toEqual(lastBitOfLastHeaderLine)
+//     expect(headerString[headerString.length - 1]).toEqual('\n')
+//     expect(headerString.length).toEqual(5315655)
+//   })
+// })
